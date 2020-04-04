bbf8c987b0475823a759f22abc1bfcce41d92a0d
embaby <engamaeae@gmail.com>
2020-04-04 19:14:22 +0200

    git commit -m"add repos interfaces"

---
+++ rev101/Models/ICoursesRepo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using rev101.Models.entites;
+
+namespace rev101.Models
+{
+    public interface ICoursesRepo
+    {
+        IEnumerator<Course> GetAllCourses();
+        IEnumerator<Course> GetCoursesOffStudent(Student student);
+        IEnumerator<Course> GetCoursesOffStudent(int studentId);
+    }
+}
